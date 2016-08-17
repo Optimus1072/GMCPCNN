1c51c9bf0088366c46c2f77748f190f19c5d89b3
Helge Wrede <helge.wrede@uni-jena.de>
2016-08-17 13:40:22 +0200

    updated doc

--- Documentation/html/search/functions_a.js
+++ Documentation/html/search/functions_a.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['normalize',['Normalize',['../classutil_1_1Filter2D.html#a242138102e2e30f4fe9cccf76f817e09',1,'util::Filter2D']]],
-  ['nstage',['NStage',['../classalgo_1_1NStage.html#a65baad453fa371f18be02fd19e695ea1',1,'algo::NStage']]]
+  ['nstage',['NStage',['../classalgo_1_1NStage.html#a3fd89ae32d8723b4fcd650be280a9c7c',1,'algo::NStage']]]
 ];
